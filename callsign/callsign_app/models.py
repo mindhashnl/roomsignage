f726c09087b678728eecdfc245cf396d969ec013
matthijs <vos1996@gmail.com>
2019-09-13 12:02:03 +0200

    Improve file uploading

--- callsign/callsign_app/models.py
+++ callsign/callsign_app/models.py
@@ -1,23 +1,15 @@
+import os
 import random
 import string
 import uuid
-from datetime import datetime, timedelta
 
 from colorfield.fields import ColorField
-from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.db import models
-from django.db.models import OneToOneField, ForeignKey
+from django.db.models import ForeignKey
 
 phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                              message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-
-pairing_code_regex = RegexValidator(regex=r'^\w{4}$', message="Pairing code should be 4 letters")
-
-
-def generate_pairing_code():
-    letters = string.ascii_letters
-    return ''.join(random.choice(letters) for i in range(4))
 
 
 def generate_secret():
@@ -25,8 +17,9 @@ def generate_secret():
     return ''.join(random.choice(letters) for i in range(32))
 
 
-def expire_default():
-    return datetime.now() + timedelta(days=2)
+def logo_upload(instance, filename):
+    name, extension = filename.split('.', 1)
+    return f"companies/{instance.id}/logos/{uuid.uuid4()}.{extension}"
 
 
 class Company(models.Model):
@@ -34,7 +27,7 @@ class Company(models.Model):
     phone_number = models.CharField(validators=[phone_regex], max_length=15)
     email = models.EmailField(max_length=50, null=False)
     website = models.URLField(max_length=50)
-    logo = models.ImageField(null=True, upload_to=f'logos/{uuid.uuid4()}')
+    logo = models.ImageField(null=True, upload_to=logo_upload)
     color = ColorField()
 
     def __str__(self):
