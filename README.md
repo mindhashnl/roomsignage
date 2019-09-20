e254ded660c8e630a8e49050a405c66e58a4356f
matthijs <vos1996@gmail.com>
2019-09-20 13:31:34 +0200

    Add test for seeds

--- README.md
+++ README.md
@@ -10,7 +10,10 @@ Now follow those steps to get everything working
 
 1. Install requirements `pip3 install -r requirements.txt`
 1. Start the database `docker-compose up -d db`
-1. Change dir to mysign `cd mysign`
 1. Setup the database `python3 manage.py migrate`
 1. Add data to database `python3 manage.py seed`
 1. Run the server `python3 manage.py runserver` (or `docker-compose up web`)
+
+## Testing
+1. Run tests `pytest --cov=mysign_app`
+1. Run linter `flake8`
---
+++ mysign_app/tests/management/test_seed.py
@@ -0,0 +1,35 @@
+from pytest import mark
+
+from mysign_app.management.commands.seed import seed
+from mysign_app.models import Company, DoorDevice, User
+
+
+@mark.django_db
+def test_company_is_seeded():
+    seed()
+
+    assert Company.objects.count() == 5
+
+
+@mark.django_db
+def test_door_device_is_seeded():
+    seed()
+
+    assert DoorDevice.objects.count() == 20
+
+
+@mark.django_db
+def test_user_is_seeded():
+    seed()
+
+    assert User.objects.filter(username='HMO').count() == 1
+    assert User.objects.filter(username='HMO').first().check_password('123456')
+
+    assert User.objects.filter(username='company').count() == 1
+    assert User.objects.filter(username='company').first().check_password('123456')
+    assert User.objects.filter(username='company').first().company_id
+
+    assert User.objects.filter(username='developer').count() == 1
+    assert User.objects.filter(username='developer').first().check_password('123456')
+    assert User.objects.filter(username='developer').first().is_staff
+    assert User.objects.filter(username='developer').first().is_superuser
