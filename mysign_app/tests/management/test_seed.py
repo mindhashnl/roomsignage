4e6a4f89c96b2858e375865914bcb552873e808b
matthijs <vos1996@gmail.com>
2019-09-20 14:03:47 +0200

    How the coverage is 100% :party:

--- mysign_app/tests/management/test_seed.py
+++ mysign_app/tests/management/test_seed.py
@@ -1,27 +1,24 @@
-from pytest import mark
+from pytest import fixture
 
-from mysign_app.management.commands.seed import seed, clear_data
+from mysign_app.management.commands.seed import Command
 from mysign_app.models import Company, DoorDevice, User
+from mysign_app.tests.factories import CompanyFactory
 
 
-@mark.django_db
+@fixture(autouse=True)
+def run_seed(db):
+    Command().handle()
+
+
 def test_company_is_seeded():
-    seed()
-
     assert Company.objects.count() == 5
 
 
-@mark.django_db
 def test_door_device_is_seeded():
-    seed()
-
     assert DoorDevice.objects.count() == 20
 
 
-@mark.django_db
 def test_user_is_seeded():
-    seed()
-
     assert User.objects.filter(username='HMO').count() == 1
     assert User.objects.filter(username='HMO').first().check_password('123456')
 
@@ -35,11 +32,10 @@ def test_user_is_seeded():
     assert User.objects.filter(username='developer').first().is_superuser
 
 
-@mark.django_db
 def test_database_is_cleared():
-    seed()
-    clear_data()
+    CompanyFactory.create(name="really awesome company")
 
-    assert Company.objects.count() == 0
-    assert DoorDevice.objects.count() == 0
-    assert User.objects.count() == 0
+    # Run seeds
+    Command().handle()
+
+    assert Company.objects.filter(name="really awesome company").count() == 0
