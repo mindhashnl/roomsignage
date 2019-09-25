ddd989b93025f6c0e0af3bb4d773ca10d0b7d03f
snaatwow <sandertb313@gmail.com>
2019-09-25 15:12:34 +0200

    change AddCompany name

--- mysign_app/urls.py
+++ mysign_app/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('admin/door_devices/', admin.door_devices, name='admin_door_devices'),
     path('admin/companies/', admin.companies, name='admin_companies'),
     path('admin/users/', admin.users, name='admin_users'),
-    path('admin/companies/addcompany', admin.addCompany, name='admin_addCompany'),
+    path('admin/companies/add/', admin.company_add, name='admin_company_add'),
 
 ]
