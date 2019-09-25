605a333583f897fe4545dead29e9d1b6f2121394
sandertb313 <s.t.tenbrinke@student.utwente.nl>
2019-09-25 12:39:08 +0200

    Changed device-overview to fit company page style (#43)
    * Changed device-overview to fit company page style
    
    * Updated code

--- mysign_app/forms.py
+++ mysign_app/forms.py
@@ -1,12 +1,18 @@
 from django.forms import ModelForm
 
-from mysign_app.models import Company, User
+from mysign_app.models import Company, User, DoorDevice
 
 
 class CompanyForm(ModelForm):
     class Meta:
         model = Company
         fields = ['name', 'email']
+
+
+class DoorDeviceForm(ModelForm):
+    class Meta:
+        model = DoorDevice
+        fields = ['company', 'id']
 
 
 class UserForm(ModelForm):
