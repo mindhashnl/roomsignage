6799a1432334e7f8cb4a116119570cb952cb4bbd
matthijs <vos1996@gmail.com>
2019-09-26 10:41:03 +0200

    Fix linting

--- mysign_app/static/mysign_app/js/datatable.js
+++ mysign_app/static/mysign_app/js/datatable.js
@@ -1,103 +1,97 @@
 /* eslint-disable-next-line no-unused-vars */
 function createDataTable(dataJson, listFields) {
+	let columns = [
+		// {   /* created column to show a picture just to make this demo look better */
+		//     "orderable": false, "data": "Photo", "name": "Photo", "defaultContent": "",
+		//     "visible": true, "className": "text-center", "width": "20px",
+		//     "createdCell": function (td, cellData, rowData, row, col) {
+		//         console.log(cellData)
+		//         var $ctl = $('<i class="fa fa-user fa-fw"></i>');
+		//         $(td).append($ctl);
+		//     }
+		// }
+	];
 
-    let columns = [
-        // {   /* created column to show a picture just to make this demo look better */
-        //     "orderable": false, "data": "Photo", "name": "Photo", "defaultContent": "",
-        //     "visible": true, "className": "text-center", "width": "20px",
-        //     "createdCell": function (td, cellData, rowData, row, col) {
-        //         console.log(cellData)
-        //         var $ctl = $('<i class="fa fa-user fa-fw"></i>');
-        //         $(td).append($ctl);
-        //     }
-        // }
-        ];
+	Object.values(listFields).forEach(function (key) {
+		columns.push({data: key, name: key});
+	});
 
-    Object.values(listFields).forEach(function (key) {
-        columns.push({data: key, name: key});
-    });
+	const table = $('#register').DataTable({
+		dom: 'fBtp', // Register plugins see https://datatables.net/reference/option/dom
+		pageLength: 10,
+		buttons: [
+			{
+				text: '<i class="fa fa-id-badge fa-fw fa-lg" aria-hidden="true"></i>',
+				action: function () {
+					$('#register').toggleClass('cards');
+					$('#card-toggle .fa').toggleClass(['fa-table', 'fa-id-badge']);
+					$('#register thead').toggle();
 
-    const table = $('#register').DataTable({
-        dom: 'fBtp', // Register plugins see https://datatables.net/reference/option/dom
-        pageLength: 10,
-        buttons: [
-            {
-                text: '<i class="fa fa-id-badge fa-fw fa-lg" aria-hidden="true"></i>',
-                action: function () {
+					if ($('#register').hasClass('cards')) {
+						// Create an array of labels containing all table headers
+						let labels = [];
+						console.log(labels);
+						$('#register').find('thead th').each(function () {
+							labels.push($(this).text());
+						});
 
-                    $("#register").toggleClass("cards");
-                    $("#card-toggle .fa").toggleClass(["fa-table", "fa-id-badge"]);
-                    $("#register thead").toggle();
+						// Add data-label attribute to each cell
+						$('#register').find('tbody tr').each(function () {
+							$(this).find('td').each(function (column) {
+								$(this).attr('data-label', labels[column]);
+							});
+						});
 
-                    if ($("#register").hasClass("cards")) {
-                        // Create an array of labels containing all table headers
-                        var labels = [];
-                        console.log(labels)
-                        $('#register').find('thead th').each(function () {
-                            labels.push($(this).text());
-                        });
+						// Set correct hight
+						let max = 0;
+						$('#register tr').each(function () {
+							max = Math.max($(this).height(), max);
+						}).height(max);
+					} else {
+						// Remove data-label attribute from each cell
+						$('#register').find('td').each(function () {
+							$(this).removeAttr('data-label');
+						});
 
-                        // Add data-label attribute to each cell
-                        $('#register').find('tbody tr').each(function () {
-                            $(this).find('td').each(function (column) {
-                                $(this).attr('data-label', labels[column]);
-                            });
-                        });
+						$('#register tr').each(function () {
+							$(this).height('auto');
+						});
+					}
+				},
+				attr: {
+					title: 'Change views',
+					id: 'card-toggle'
+				}
+			}
+		],
+		select: 'single',
+		data: dataJson,
+		columns: columns,
+		fnInitComplete: function () {
+			$('#card-toggle').click();
+			$('#register thead').hide();
+		}
+	})
 
-                        // set correct hight
-                        let max = 0;
-                        $('#register tr').each(function () {
-                            max = Math.max($(this).height(), max);
-                        }).height(max);
-                    } else {
-                        // Remove data-label attribute from each cell
-                        $('#register').find('td').each(function () {
-                            $(this).removeAttr('data-label');
-                        });
+		.on('select', function (e, dt, type, indexes) {
+			let rowData = table.rows(indexes).data().toArray();
 
-                        $("#register tr").each(function () {
-                            $(this).height("auto");
-                        });
-                    }
-                },
-                attr: {
-                    title: 'Change views',
-                    id: 'card-toggle'
-                }
-            }
-        ],
-        select: 'single',
-        data: dataJson,
-        columns: columns,
-        fnInitComplete: function () {
-            $('#card-toggle').click();
-            $('#register thead').hide();
+			// For all labels
+			for (const [key, value] of Object.entries(rowData[0])) {
+				// If field is bool, set checkbox
+				if (value === true || value === false) {
+					let fieldName = '#id_' + key;
+					$(fieldName).prop('checked', value);
+				} else
 
-        },
-    })
-
-    .on('select', function (e, dt, type, indexes) {
-        let rowData = table.rows(indexes).data().toArray();
-
-        //For all labels
-        for (const [key, value] of Object.entries(rowData[0])) {
-
-            //if field is bool, set checkbox
-            if (value === true ||  value === false){
-                fieldName = "#id_" + key;
-                $(fieldName).prop('checked', value)
-            } else
-                //If key is id, set the id field
-                if (key === 'id') {
-                $(id).val(value)
-            }
-                //Else, set the field with the #id_FIELDNAME id.
-            else {
-                fieldName = "#id_" + key;
-                $(fieldName).val(value)
-            }
-        }
-
-
-    })
+				// If key is id, set the id field
+				if (key === 'id') {
+					$('id').val(value);
+				} else {
+					// Else, set the field with the #id_FIELDNAME id.
+					let fieldName = '#id_' + key;
+					$(fieldName).val(value);
+				}
+			}
+		});
 }
