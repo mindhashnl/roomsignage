a720313c5e4fe4cdc234bae33bfee46de98bda36
Matthijs Vos <vos1996@gmail.com>
2019-09-26 12:42:20 +0200

    Apply admin redirect based on role (#53)

--- mysign_app/urls.py
+++ mysign_app/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
 
     path('admin/door_devices/', admin.door_devices, name='admin_door_devices'),
     path('admin/companies/', admin.companies, name='admin_companies'),
-    path('admin/users/', admin.users, name='admin_users'),
+    path('admin/company', admin.company_add, name='admin_company'),  # TODO link this to the company page
     path('admin/companies/add/', admin.company_add, name='admin_company_add'),
+    path('admin/users/', admin.users, name='admin_users'),
 
 ]
