9a883b2c28d6d9ae4e830ce7cca6bd8a9af00e41
matthijs <vos1996@gmail.com>
2019-09-26 09:58:25 +0200

    Add save logic

--- mysign_app/models.py
+++ mysign_app/models.py
@@ -37,6 +37,12 @@ class User(AbstractUser):
             raise ValidationError("Company and is_admin cannot set both")
 
     def save(self, *args, **kwargs):
+        # If this is a new user
+        if not self.pk and not self.password:
+            is_new = True
+            password = User.objects.make_random_password()
+            self.set_password(password)
+
         self.full_clean()
         super().save(*args, **kwargs)
 
--- mysign_app/routes/admin.py
+++ mysign_app/routes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 from mysign_app.forms import CompanyForm, UserForm, DoorDeviceForm, AddCompanyUserForm
 from mysign_app.models import DoorDevice, Company, User
@@ -58,9 +59,21 @@ def users(request):
 
 @admin_required
 def company_add(request):
+    if request.method == 'POST':
+        company_form = CompanyForm(request.POST, prefix='company')
+        user_form = AddCompanyUserForm(request.POST, prefix='user')
+        if company_form.is_valid() and user_form.is_valid():
+            company_form.save()
+            user_form.save()
+            messages.info(request, 'Company and user successfully added')
+            return redirect('admin_companies')
+    else:
+        company_form = CompanyForm(prefix='company')
+        user_form = AddCompanyUserForm(prefix='user')
+
     template = loader.get_template('mysign_app/admin/company_add.html')
     context = {
-        'user_form': AddCompanyUserForm(),
-        'company_form': CompanyForm(),
+        'user_form': user_form,
+        'company_form': company_form,
     }
     return HttpResponse(template.render(context, request))
