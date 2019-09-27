c6a20cbd7c7d2ee0cc2ba6e4b9b2d3d38531ec89
matthijs <vos1996@gmail.com>
2019-09-27 11:42:35 +0200

    Improve deployment

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker build -t mysign .
+docker build -t mysign . # Build
+
+cd /proj
+docker-compose run --rm mysign python3 manage.py migrate # Run migrations
+docker-compose up -d web # Restart
