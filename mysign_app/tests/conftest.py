44e20c86c3f95c198dabcd92b59400bec944a518
matthijs <vos1996@gmail.com>
2019-10-01 15:35:38 +0200

    WIP

--- mysign_app/tests/conftest.py
+++ mysign_app/tests/conftest.py
@@ -1,5 +1,5 @@
 # Make sure that the helper method asserts are handled as pytest asserts
-import pytest
-
-pytest.register_assert_rewrite("mysign_app.tests.routes.authentication_helpers")
-pytest.register_assert_rewrite("mysign_app.tests.routes.form_helpers")
+# import pytest
+#
+# pytest.register_assert_rewrite("mysign_app.tests.routes.authentication_helpers")
+# pytest.register_assert_rewrite("mysign_app.tests.routes.form_helpers")
--- mysign_app/tests/routes/test_company.py
+++ mysign_app/tests/routes/test_company.py
@@ -1,9 +1,25 @@
 from django.urls import reverse
 from pytest import mark
 
-from mysign_app.tests.routes.authentication_helpers import is_company_route
+from mysign_app.models import Company
+from mysign_app.tests.factories import CompanyFactory
+from mysign_app.tests.routes.authentication_helpers import is_company_route, client_login
+from mysign_app.tests.routes.form_helpers import payload_from_form
 
 
 @mark.django_db
 def test_index(client):
     is_company_route(client, reverse('company_index'))
+
+
+# @mark.django_db
+# def test_update(client):
+#     company = CompanyFactory()
+#     client_login(client, company=company)
+#
+#     company.name = 'New name'
+#     payload = payload_from_form(CompanyFactory(instance=company))
+#     response = client.post(reverse('admin_index'), payload)
+#
+#     assert response.status_code == 200
+#     assert Company.objects.get(id=company.id).name == 'New name'
