a6e0dc3f8848506cd7b3cadac9aa9a658b687c9a
WoudstraITSolutions <martijnhenriwoudstra@gmail.com>
2019-10-01 15:55:05 +0200

    remove prints

--- mysign_app/mail/mail_sender.py
+++ mysign_app/mail/mail_sender.py
@@ -6,11 +6,6 @@ def send_mail(from_email, to_emails, subject, html_content):
     message = Mail(from_email, to_emails, subject, html_content)
     try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-        print(sg)
-        print(os.environ.get('SENDGRID_API_KEY'))
-        response = sg.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        sg.send(message)
     except Exception as e:
         print(e.message)
