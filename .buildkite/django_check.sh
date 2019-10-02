686e4a6d08008a2f3e5a5e84215a6127ef3c87fa
matthijs <vos1996@gmail.com>
2019-10-02 11:08:48 +0200

    Check that all models are reflected in migrations

---
+++ .buildkite/django_check.sh
@@ -0,0 +1,7 @@
+python3 manage.py check
+python3 manage.py makemigrations --check
+if [[ $? == 0 ]]; then
+    echo "All models are reflected in migrations"
+else
+    echo "ERROR: Not all models are relfected in migrations. Please run manage.py makemigrations"
+fi
