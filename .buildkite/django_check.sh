f23a90d3406a9ea68bbfd8fdfadd9751ea480288
matthijs <vos1996@gmail.com>
2019-10-02 11:09:47 +0200

    Add exit

--- .buildkite/django_check.sh
+++ .buildkite/django_check.sh
@@ -4,4 +4,5 @@ if [[ $? == 0 ]]; then
     echo "All models are reflected in migrations"
 else
     echo "ERROR: Not all models are relfected in migrations. Please run manage.py makemigrations"
+    exit 1
 fi
