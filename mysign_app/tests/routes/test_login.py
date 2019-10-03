8b71ddad450775c5dad740d06327e74b840f9601
matthijs <vos1996@gmail.com>
2019-10-03 13:32:15 +0200

    Fix tests

--- mysign_app/tests/routes/test_login.py
+++ mysign_app/tests/routes/test_login.py
@@ -8,9 +8,8 @@ from mysign_app.tests.routes.helpers import messages_to_list
 
 
 def create_user(**kwargs):
-    User.objects.create_user(username='test_user',
+    User.objects.create_user(email='info@example.com',
                              password='123456',
-                             email='info@example.com',
                              **kwargs)
 
 
@@ -18,7 +17,7 @@ def create_user(**kwargs):
 def test_login_wrong_credentials(client):
     create_user()
 
-    response = client.post(reverse('login'), {'username': 'test_user', 'password': 'wrong_password!'})
+    response = client.post(reverse('login'), {'username': 'info@example.com', 'password': 'wrong_password!'})
 
     assert response.status_code == 200
     assert '_auth_user_id' not in client.session
@@ -29,7 +28,7 @@ def test_login_correct_credentials(client):
     """ When with correct credentials but no staff/company/admin configured login is not possible"""
     create_user()
 
-    response = client.post(reverse('login'), {'username': 'test_user', 'password': '123456'})
+    response = client.post(reverse('login'), {'username': 'info@example.com', 'password': '123456'})
     messages = messages_to_list(response)
 
     assert response.status_code == 302
@@ -42,7 +41,7 @@ def test_login_correct_credentials(client):
 def test_login_admin(client):
     create_user(is_admin=True)
 
-    response = client.post(reverse('login'), {'username': 'test_user', 'password': '123456'})
+    response = client.post(reverse('login'), {'username': 'info@example.com', 'password': '123456'})
 
     assert response.status_code == 302
     assert response.url == reverse('admin_door_devices')
@@ -53,7 +52,7 @@ def test_login_company(client):
     company = CompanyFactory()
     create_user(company=company)
 
-    response = client.post(reverse('login'), {'username': 'test_user', 'password': '123456'})
+    response = client.post(reverse('login'), {'username': 'info@example.com', 'password': '123456'})
 
     assert response.status_code == 302
     assert response.url == reverse('company_index')
@@ -63,7 +62,7 @@ def test_login_company(client):
 def test_login_staff(client):
     create_user(is_staff=True)
 
-    response = client.post(reverse('login'), {'username': 'test_user', 'password': '123456'})
+    response = client.post(reverse('login'), {'username': 'info@example.com', 'password': '123456'})
 
     assert response.status_code == 302
     assert response.url == '/django_admin'
