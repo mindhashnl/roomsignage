064191ce4aca311363ed3c2f1619bd4c3a63002c
matthijs <vos1996@gmail.com>
2019-10-28 12:19:29 +0100

    Add testing

--- mysign_app/tests/frontend/admin/test_user_overview.py
+++ mysign_app/tests/frontend/admin/test_user_overview.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from pytest import fixture
 from selenium.webdriver.common.keys import Keys
 
@@ -143,6 +145,28 @@ def test_invalid_save(selenium):
     error_card = selenium.find_element_by_xpath("//div[@class='alert alert-block alert-danger']")
     assert error_card
     assert error_card.text == 'Company and is_admin cannot set both'
+
+
+def test_remove_button(selenium):
+    cards = selenium.find_elements_by_xpath("//td[@class='name sorting_1']")
+    # check that the total amount of cards is 2
+    assert len(cards) == 3
+
+    # select the card and press delete
+    cards[0].click()
+    selenium.find_element_by_id("deleteButton").click()
+
+    # check chrome popup, click "Cancel"
+    selenium.switch_to.alert.dismiss()
+    cards = selenium.find_elements_by_xpath("//td[@class='name sorting_1']")
+    assert len(cards) == 3
+
+    selenium.find_element_by_id("deleteButton").click()
+
+    # check chrome popup, click Accept"
+    selenium.switch_to.alert.accept()
+    cards = selenium.find_elements_by_xpath("//td[@class='name sorting_1']")
+    assert len(cards) == 2
 
 
 def test_search(selenium):
