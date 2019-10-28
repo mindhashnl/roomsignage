1ef59a1a3fedff8fc2fb6e1e175eb3d039f1eb57
sandertb313 <s.t.tenbrinke@student.utwente.nl>
2019-10-28 10:27:25 +0100

    Feature/selenium/user/add (#168)
    * no message
    
    * Update test_go.py
    
    * Added login and logout test
    
    logout test not working yet
    
    * Reformat
    
    * More cleanup
    
    * updated
    
    * WWIP
    
    * Update test_go.py
    
    * Added Id's to navbar, updated test
    
    * Moved into other package
    
    * Add some tests
    
    * WIP
    
    * WIP logout fixed
    
    * test_navbar done
    
    * fixed typo
    
    * Fixes
    
    * Delete db
    
    * updated logout_test
    
    * Fixed logout
    
    * Fixed base.py
    
    * req.txt updated
    
    * Fixes
    
    * move it
    
    * Update helpers.py
    
    * add headless
    
    * Flake
    
    * Added test_companies, commented chrome_options out
    
    Last push on this branch
    
    * Added test_device_overview
    
    PK counter does not reset if the database is reset...... So id's are not as pretty as they should be
    
    * Created test_user_overview
    
    * tests for add_user page
    
    * Fixed merge

---
+++ mysign_app/tests/frontend/admin/test_add_user.py
@@ -0,0 +1,115 @@
+from pytest import fixture
+
+from mysign_app.models import Company
+from mysign_app.tests.frontend.helpers import authenticate_selenium
+
+
+@fixture(autouse=True)
+def setup_user_test(selenium, live_server):
+    Company.objects.create(name="Mindhash", email="info@mindhash.com")
+    Company.objects.create(name="Test", email="test@test.com")
+
+    authenticate_selenium(selenium, live_server, is_admin=True, first_name="admin")
+    selenium.maximize_window()
+    selenium.get(live_server.url + "/admin/users/add")
+
+
+def fill_form(selenium):
+    selenium.find_element_by_id('id_user-first_name').send_keys("Test")
+    selenium.find_element_by_id('id_user-last_name').send_keys("One")
+    selenium.find_element_by_id('id_user-email').send_keys("test@one.com")
+    selenium.find_element_by_xpath("// select[ @ id = 'id_user-company'] / option[text() = 'Mindhash']").click()
+    selenium.find_element_by_id('id_user-is_admin').click()
+
+
+# tests if the form is clear when page is loaded
+def test_form_clear(selenium):
+    assert selenium.find_element_by_id('id_user-first_name').get_attribute('value') == ''
+    assert selenium.find_element_by_id('id_user-last_name').get_attribute('value') == ''
+    assert selenium.find_element_by_id('id_user-email').get_attribute('value') == ''
+    assert selenium.find_element_by_id('id_user-company').get_attribute('value') == ''
+    assert not selenium.find_element_by_id('id_user-is_admin').is_selected()
+
+
+# tests save button and adding of user
+def test_save(selenium, live_server):
+    selenium.get(live_server.url + "/admin/users")
+
+    # starting amount of user cards
+    cards_not_active = selenium.find_elements_by_xpath("//td[@class='name sorting_1']")
+    cards_active = selenium.find_elements_by_xpath("//td[@class='name active sorting_1']")
+    assert len(cards_not_active) + len(cards_active) == 1
+
+    selenium.get(live_server.url + "/admin/users/add")
+    fill_form(selenium)
+    # deselect is_admin
+    selenium.find_element_by_id('id_user-is_admin').click()
+    # click submit
+    selenium.find_element_by_id('submit_button').click()
+    # user should be added successfully, so redirect to user page is expected
+    assert selenium.current_url == live_server.url + "/admin/users/"
+
+    # since a user has been added, a card should also have been added
+    cards_not_active = selenium.find_elements_by_xpath("//td[@class='name sorting_1']")
+    cards_active = selenium.find_elements_by_xpath("//td[@class='name active sorting_1']")
+    assert len(cards_not_active) + len(cards_active) == 2
+
+
+# tests if a user can be added without email
+def test_save_no_email(selenium, live_server):
+    fill_form(selenium)
+    selenium.find_element_by_id('id_user-email').clear()
+
+    selenium.find_element_by_id('submit_button').click()
+    # this should be denied, so the current page should still be the add user page
+    assert selenium.current_url == live_server.url + "/admin/users/add"
+
+
+# tests if a user can be added with both a company and as admin
+def test_save_company_admin(selenium, live_server):
+    fill_form(selenium)
+
+    selenium.find_element_by_id('submit_button').click()
+    # this should be denied, so the current page should still be the add user page
+    assert selenium.current_url == live_server.url + "/admin/users/add"
+
+
+# Tests if a user can be added with only is_admin
+def test_save_no_company_admin(selenium, live_server):
+    fill_form(selenium)
+
+    selenium.find_element_by_xpath("// select[ @ id = 'id_user-company'] / option[text() = '---------']").click()
+    selenium.find_element_by_id('submit_button').click()
+    assert selenium.current_url == live_server.url + "/admin/users/"
+
+
+# Tests if a user can be saved with only company selected and not admin
+def test_save_company_no_admin(selenium, live_server):
+    fill_form(selenium)
+
+    selenium.find_element_by_id('id_user-is_admin').click()
+    selenium.find_element_by_id('submit_button').click()
+    assert selenium.current_url == live_server.url + "/admin/users/"
+
+
+# tests cancel button
+def test_cancel(selenium, live_server):
+    selenium.get(live_server.url + "/admin/users")
+
+    # starting amount of cards
+    cards_not_active = selenium.find_elements_by_xpath("//td[@class='name sorting_1']")
+    cards_active = selenium.find_elements_by_xpath("//td[@class='name active sorting_1']")
+    assert len(cards_not_active) + len(cards_active) == 1
+
+    selenium.get(live_server.url + "/admin/users/add")
+    fill_form(selenium)
+
+    # click cancel
+    selenium.find_element_by_id('cancel_button').click()
+    # adding should be cancelled, so redirect to user page is expected
+    assert selenium.current_url == live_server.url + "/admin/users/"
+
+    # since no user should have been added, the amouont of cards should still be one
+    cards_not_active = selenium.find_elements_by_xpath("//td[@class='name sorting_1']")
+    cards_active = selenium.find_elements_by_xpath("//td[@class='name active sorting_1']")
+    assert len(cards_not_active) + len(cards_active) == 1
