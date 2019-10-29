8f6cbf46b92c93ee9cac93652d2b2cf633ca6673
matthijs <vos1996@gmail.com>
2019-10-29 12:16:17 +0100

    Do full restart

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#docker build -t mysign . # Build
+docker build -t mysign . # Build
 
 cd /proj
 docker-compose run --rm web python3 manage.py migrate # Run migrations
 docker-compose run -v ${PWD}/static:/static --rm web python3 manage.py collectstatic --noinput # Collect static files, such that Nginx can serve
-docker-compose up -d web # Restart
+docker-compose up -d # Restart
